e78a1ca5b720513bc2d170c1a05415b36abb8270
ydang <lapluga03@gmail.com>
2020-12-22 18:18:12 +0700

    apply page object model for choose received place

--- src/Pages/DMXChooseReceivedPlace.java
+++ src/Pages/DMXChooseReceivedPlace.java
@@ -1,24 +1,24 @@
-package TestCases;
+package Pages;
 
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.Reporter;
-import org.testng.annotations.*;
-import AbstractAnnotation.*;
-import ActionsForITestListener.*;
-import Enum.*;
-
-import static org.testng.Assert.expectThrows;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class ChooseReceivedPlace extends AbstractClass {
+import AbstractAnnotation.AbstractClass;
+import ActionsForITestListener.ExcelUtils;
+import Enum.Result;
+
+public class DMXChooseReceivedPlace extends AbstractClass {
 	public static final int COL_TESTTYPE = 0;
 	public static final int COL_TESTNAME = 1;
 	public static final int COL_CASE = 2;
@@ -39,11 +39,7 @@ public class ChooseReceivedPlace extends AbstractClass {
 		WebElement provincesBox = waitForElementVisible(By.xpath("//div[@class='provinces-box']//child::span"));
 		provincesBox.click();
 	}
-
-	/*
-	 * Test requirement: TR-DMX-CNNH-01 - Test case: TC-DMX-CNNH-01
-	 */
-	@Test(priority = 1, groups = { "chooseReceivePlaceSuccess" })
+	@Test(priority = 1)
 	public void testMainSFWithFullInfor() throws Exception {
 		
 		rowData = ExcelUtils.getRowContains("testMainSFWithFullInfor", COL_TESTNAME);
@@ -73,10 +69,6 @@ public class ChooseReceivedPlace extends AbstractClass {
 			ExcelUtils.setCellData(rowData, COL_RESULT, Result.FAILED.toString());
 		}
 	}
-
-	/*
-	 * Test requirement: TR-DMX-CNNH-01 - Test case: TC-DMX-CNNH-02
-	 */
 	@Test(priority = 2, groups = { "chooseRecivePlaceSuccess" })
 	public void testSuccessWithOnlyProvince() throws Exception {
 		rowData = ExcelUtils.getRowContains("testSuccessWithOnlyProvince", COL_TESTNAME);
@@ -306,109 +298,127 @@ public class ChooseReceivedPlace extends AbstractClass {
 			ExcelUtils.setCellData(rowData, COL_RESULT, Result.FAILED.toString());
 		}
 	}
-
-	private void chooseProvinceBox() {
+	
+	By chooseReceivedPlaceBox = By.xpath("//div[@class='provinces-box']//child::span");
+	
+	By dropBoxProvince = By.id("location_listPro");
+	By allListProvince = By.xpath("//div[@class='flex']//ul");
+	
+	By dropBoxDistrict = By.id("location_listDistrict");
+	By allListDistrict = By.id("lstDistrict");
+	By styleBoxDistrict = By.xpath("//div[@class='boxprov__listTT--scroll']");
+	
+	By dropBoxWard = By.id("location_listWard");
+	By allListWard = By.id("lstWard");
+	By errorWard = By.className("errWard");
+	
+	By textBoxAddress = By.id("locationAddress");
+	
+	By buttonSubmit = By.id("lc_btn-Confirm");
+	By buttonChangeLocation = By.xpath("//a[@id='lc_btn-changeLc']");
+	
+	By list = By.tagName("li");
+	By eachItem = By.tagName("a");
+	
+	public List<WebElement> getList(By locator){
+		return waitForElementVisible(locator).findElements(list);
+	}
+	
+	public List<WebElement> getStyleWard(){
+		return driver.findElements(styleBoxDistrict);
+	}
+	public void chooseProvinceBox() {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		WebElement provincesBox = waitForElementVisible(By.xpath("//div[@class='provinces-box']//child::span"));
-		provincesBox.click();
+		waitForElementVisible(chooseReceivedPlaceBox).click();
 	}
 
-	private void fillAddress(String address) {
-		WebElement fillAddress = waitForElementClickable(By.id("locationAddress"));
-		fillAddress.sendKeys(address);
+	public void fillAddress(String address) {
+		waitForElementClickable(textBoxAddress).sendKeys(address);
 	}
 
-	private void clickOnWardDropBox() {
-		WebElement dropboxWard = waitForElementClickable(By.id("location_listWard"));
-		dropboxWard.click();
+	public void clickOnWardDropBox() {
+		waitForElementClickable(dropBoxWard).click();
 	}
 
-	private void submitForm() throws InterruptedException {
-		WebElement submit = waitForElementClickable(By.id("lc_btn-Confirm"));
-		submit.click();
+	public void submitForm() throws InterruptedException {
+		waitForElementClickable(buttonSubmit).click();
 		Thread.sleep(1000);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
-	private void chooseProvince(String province) throws InterruptedException {
-		WebElement setProvince = waitForElementVisible(By.id("location_listPro"));
-		setProvince.click();
+	public void chooseProvince(String province) throws InterruptedException {
+		waitForElementVisible(dropBoxProvince).click();
 		Thread.sleep(1000);
-		WebElement divContainsUl = waitForElementVisible(By.xpath("//div[@class='flex']//ul"));
-		List<WebElement> lis = divContainsUl.findElements(By.tagName("li"));
-		for (int i = 0; i < lis.size(); i++) {
-			if (lis.get(i).findElement(By.tagName("a")).getText().contentEquals(province)) {
-				lis.get(i).click();
+		for (int i = 0; i < getList(allListProvince).size(); i++) {
+			if (getList(allListProvince).get(i).findElement(eachItem).getText().contentEquals(province)) {
+				getList(allListProvince).get(i).click();
 				break;
 			}
 		}
 	}
 
-	private void chooseDistrict(String district) throws InterruptedException {
+	public void clickOnDistrictDropBox() {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		List<WebElement> scrollboxDistrict = driver.findElements(By.xpath("//div[@class='boxprov__listTT--scroll']"));
-		String getStyle = scrollboxDistrict.get(2).getAttribute("style");
+		String getStyle = getStyleWard().get(2).getAttribute("style");
 		if (getStyle.equals("display: none;") || getStyle.equals("")) {
-			WebElement setDistrict = waitForElementClickable(By.id("location_listDistrict"));
-			setDistrict.click();
+			waitForElementClickable(dropBoxDistrict).click();
 		}
-		WebElement divContainsLi = waitForElementVisible(By.id("lstDistrict"));
-		List<WebElement> lis = divContainsLi.findElements(By.xpath("//div[@id='lstDistrict']//child::li"));
-		for (int i = 0; i < lis.size(); i++) {
-			if (lis.get(i).findElement(By.tagName("a")).getText().contentEquals(district)) {
-				lis.get(i).click();
+	}
+	public void chooseDistrict(String district) throws InterruptedException {
+		clickOnDistrictDropBox();
+		//waitForElementClickable(dropBoxDistrict).click();
+		for (int i = 0; i < getList(allListDistrict).size(); i++) {
+			if (getList(allListDistrict).get(i).findElement(eachItem).getText().contentEquals(district)) {
+				getList(allListDistrict).get(i).click();
 				break;
 			}
 		}
 	}
 
-	private void chooseWard(String ward) {
-		WebElement divContainsLi = waitForElementVisible(By.id("lstWard"));
-		List<WebElement> lis = divContainsLi.findElements(By.xpath("//div[@id='lstWard']//child::li"));
-		for (int i = 0; i < lis.size(); i++) {
-			if (lis.get(i).findElement(By.tagName("a")).getText().contentEquals(ward)) {
-				lis.get(i).click();
+	public void chooseWard(String ward) {
+		for (int i = 0; i < getList(allListWard).size(); i++) {
+			if (getList(allListWard).get(i).findElement(eachItem).getText().contentEquals(ward)) {
+				getList(allListWard).get(i).click();
 				break;
 			}
 		}
 	}
 
-	private void changeInforOnTextBox(String address) throws AWTException, InterruptedException {
-		WebElement locationTextBox = waitForElementClickable(By.id("locationAddress"));
-		locationTextBox.click();
+	public void changeInforOnTextBox(String address) throws AWTException, InterruptedException {
+		waitForElementClickable(textBoxAddress).click();
 		final Robot robot = new Robot();
 		Thread.sleep(2000);
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_A);
 		robot.keyRelease(KeyEvent.VK_CONTROL);
 		robot.keyRelease(KeyEvent.VK_A);
-		locationTextBox.sendKeys(address);
+		waitForElementVisible(textBoxAddress).sendKeys(address);
 	}
 
-	private void clickButtonChangeLc() {
-		WebElement btnChangeLc = waitForElementClickable(By.xpath("//a[@id='lc_btn-changeLc']"));
-		btnChangeLc.click();
+	public void clickButtonChangeLc() {
+		waitForElementClickable(buttonChangeLocation).click();
 	}
 
-	private String getActualResult() throws InterruptedException {
+	public String getActualResult() throws InterruptedException {
 		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Thread.sleep(1000);
-		String actualResult = (waitForElementVisible(By.xpath("//div[@class='provinces-box']//child::span"))).getText();
+		String actualResult = (waitForElementVisible(chooseReceivedPlaceBox)).getText();
 		return actualResult;
 	}
 
-	private String getActualErrorWard() throws InterruptedException {
-		String actualErr = (waitForElementVisible(By.className("errWard"))).getText();
+	public String getActualErrorWard() throws InterruptedException {
+		String actualErr = (waitForElementVisible(errorWard)).getText();
 		return actualErr;
 	}
 
-	private WebElement waitForElementClickable(By locator) {
+	public WebElement waitForElementClickable(By locator) {
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		return wait.until(ExpectedConditions.elementToBeClickable(locator));
 	}
 
-	private WebElement waitForElementVisible(By locator) {
+	public WebElement waitForElementVisible(By locator) {
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
+
 }
